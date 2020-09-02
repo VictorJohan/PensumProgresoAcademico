62433893fa0e56190e915cc9bbb2e2bc0961d6f4
Victor Johan <vicjohan23@gmail.com>
2020-09-02 03:50:05 -0400

    SE LE AGREGO UNA PRESENTACION.

---
+++ Presentacion.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PensumProgresoAcademico
+{
+    /// <summary>
+    /// Interaction logic for Presentacion.xaml
+    /// </summary>
+    public partial class Presentacion : Window
+    {
+        public Presentacion()
+        {
+            InitializeComponent();
+
+            //Iniciar();
+        
+        }
+
+        async public void Iniciar()
+        {
+            await Task.Delay(5000);
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
