d2422274e81b56da765222b4e957b39a7adc810c
Victor Johan <vicjohan23@gmail.com>
2020-09-02 00:15:00 -0400

    SE CREO UN MENU DESPLEGABLE PERO AUN NO SE LE ASIGNAN LOS EVENTOS.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PensumProgresoAcademico.UI.Consultas.cEstudiantes;
+﻿using MaterialDesignThemes.Wpf;
+using PensumProgresoAcademico.UI.Consultas.cEstudiantes;
 using PensumProgresoAcademico.UI.Consultas.cInscripciones;
 using PensumProgresoAcademico.UI.Consultas.cMaterias;
 using PensumProgresoAcademico.UI.Consultas.cPensum;
@@ -6,6 +7,7 @@ using PensumProgresoAcademico.UI.Registros;
 using PensumProgresoAcademico.UI.Registros.rInscripciones;
 using PensumProgresoAcademico.UI.Registros.rMaterias;
 using PensumProgresoAcademico.UI.Registros.rPensum;
+using PensumProgresoAcademico.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,8 +34,30 @@ namespace PensumProgresoAcademico
         public MainWindow()
         {
             InitializeComponent();
+
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("Materia"));
+            menuRegister.Add(new SubItem("Pensum"));
+            menuRegister.Add(new SubItem("Estudiante"));
+            menuRegister.Add(new SubItem("Inscrinción"));
+            var item = new ItemMenu("Registros", menuRegister, PackIconKind.Register);
+
+            var menuConsulta = new List<SubItem>();
+            menuConsulta.Add(new SubItem("Materia"));
+            menuConsulta.Add(new SubItem("Pensum"));
+            menuConsulta.Add(new SubItem("Estudiante"));
+            menuConsulta.Add(new SubItem("Inscrinción"));
+            var item2 = new ItemMenu("Consultas", menuConsulta, PackIconKind.Search);
+
+           
+            MenuStackPanel.Children.Add(new UserControlMenuItem(item));
+            MenuStackPanel.Children.Add(new UserControlMenuItem(item2));
+            
+            
+
+
         }
 
-        
+
     }
 }
---
+++ UserControlMenuItem.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PensumProgresoAcademico.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PensumProgresoAcademico
+{
+    /// <summary>
+    /// Interaction logic for UserControlMenuItem.xaml
+    /// </summary>
+    public partial class UserControlMenuItem : UserControl
+    {
+        public UserControlMenuItem(ItemMenu itemMenu)
+        {
+            InitializeComponent();
+            ExpandeMenu.Visibility = itemMenu.SubItems == null ? Visibility.Collapsed : Visibility.Visible;
+            ListViewItemMenu.Visibility = itemMenu.SubItems == null ? Visibility.Visible : Visibility.Collapsed;
+
+            this.DataContext = itemMenu;
+        }
+    }
+}
